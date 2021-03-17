87f4103291e1ca8c1e2dc72fce4122842d825041
Arc Warden <wardenswap@gmail.com>
2021-03-17 17:29:09 +0700

    remove duplicated license

--- flatten/Timelock.sol
+++ flatten/Timelock.sol
@@ -194,8 +194,6 @@ library SafeMath {
 
 // File: contracts/Timelock.sol
 
-// SPDX-License-Identifier: MIT
-
 // COPIED FROM https://github.com/compound-finance/compound-protocol/blob/master/contracts/Governance/GovernorAlpha.sol
 // Copyright 2020 Compound Labs, Inc.
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
